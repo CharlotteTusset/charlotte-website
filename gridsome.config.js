02562f3a6c19aaadfc9fb8a72a90cbda3dd8e5df
Charlotte <charlotte.tusset@gmail.com>
2021-03-28 18:28:39 +0200

    Add Plugins and Structure

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,8 +3,41 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        // if you use scss
+        path.resolve(__dirname, './src/assets/sass/global.scss'),
+      ],
+    })
+}
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    // or if you use scss
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  },
+  siteName: 'Charlotte Tusset',
+  siteDescription: 'Charlotte\'s journey as a frontend developer and what drives her forward',
+  siteUrl: 'https://charlottetusset.me',
+  plugins: [
+    {
+      use: 'gridsome-plugin-robots-txt',
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-127662345-1'
+      }
+    },
+
+  ]
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '~/assets/sass/global.scss'
 
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
+  // Styling
+  Vue.use(BootstrapVue)
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
